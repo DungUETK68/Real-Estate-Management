88ea84ab5aba56b1c8df183cfac0ddb0469827f3
DungUETK68 <greenkalos@gmail.com>
2026-03-26 16:51:59 +0700

    Add post, put, delete method

--- src/main/java/com/website/api/BuildingAPI.java
+++ src/main/java/com/website/api/BuildingAPI.java
@@ -3,25 +3,37 @@ package com.website.api;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.website.model.BuildingDTO;
+import com.website.model.BuildingRequestDTO;
+import com.website.repository.entity.BuildingEntity;
+import com.website.repository.entity.DistrictEntity;
 import com.website.service.BuildingService;
 
 import customexception.FieldRequiredException;
 
 @RestController
+@Transactional
 public class BuildingAPI {
 	@Autowired
 	private BuildingService buildingService;
-
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@GetMapping(value = "/api/building")
 	public List<BuildingDTO> getBuildings(@RequestParam Map<String, Object> params,
 										@RequestParam(name="typeCode", required = false) List<String> typeCode) {
@@ -36,13 +48,35 @@ public class BuildingAPI {
 	}
 	
 	@PostMapping(value = "/api/building")
-	public Object postBuildings(@RequestBody BuildingDTO building) {
-		valiDate(building);
-		return null;
+	public void postBuilding(@RequestBody BuildingRequestDTO buildingRequestDTO) {
+		BuildingEntity buildingEntity = new BuildingEntity();
+		buildingEntity.setName(buildingRequestDTO.getName());
+		buildingEntity.setNumberOfBasement(buildingRequestDTO.getNumberOfBasement());
+		buildingEntity.setWard(buildingRequestDTO.getWard());
+		buildingEntity.setStreet(buildingRequestDTO.getStreet());
+		DistrictEntity districtEntity = new DistrictEntity();
+		districtEntity.setId(buildingRequestDTO.getDistrictId());
+		buildingEntity.setDistrict(districtEntity);
+		entityManager.persist(buildingEntity);
+	}
+	
+	@PutMapping(value = "/api/building")
+	public void putBuilding(@RequestBody BuildingRequestDTO buildingRequestDTO) {
+		BuildingEntity buildingEntity = new BuildingEntity();
+		buildingEntity.setId(20L);
+		buildingEntity.setName(buildingRequestDTO.getName());
+		buildingEntity.setNumberOfBasement(buildingRequestDTO.getNumberOfBasement());
+		buildingEntity.setWard(buildingRequestDTO.getWard());
+		buildingEntity.setStreet(buildingRequestDTO.getStreet());
+		DistrictEntity districtEntity = new DistrictEntity();
+		districtEntity.setId(buildingRequestDTO.getDistrictId());
+		buildingEntity.setDistrict(districtEntity);
+		entityManager.merge(buildingEntity);
 	}
 	
 	@DeleteMapping(value = "/api/building/{id}")
-	public void deleteBuilding(@PathVariable int id) {
-		System.out.print("Da xoa " + id);
+	public void deleteBuilding(@PathVariable Long id) {
+		BuildingEntity buildingEntity = entityManager.find(BuildingEntity.class, id);
+		entityManager.remove(buildingEntity);
 	}
 }
